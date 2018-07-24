5b44481d93fa93c29c7e0614bad78f5dcb60f06e
Nayan Shah <nashah@microsoft.com>
2018-07-23 19:31:36 -0700

    Pass color correctly to all methods

--- display/Source.cpp
+++ display/Source.cpp
@@ -50,7 +50,7 @@ class IClockFace
 {
 public:
     virtual ~IClockFace() {}
-    virtual void draw(struct tm const &time, int color) = 0;
+    virtual void draw(struct tm const &time, uint16_t color) = 0;
 };
 
 class StandardClockFace : public IClockFace
@@ -58,27 +58,32 @@ class StandardClockFace : public IClockFace
     Adafruit_GFX &display;
     int16_t w, h;
     double mid_x, mid_y;
+    int minorHalfSize;
+    double hour, minute, second;
+
     static const int16_t DegHour = 30, DegMin = 6, DegSec = 6;
 
-    void DrawHand(double degrees, double length, bool thick = false, int color = BLACK);
+    void DrawHand(double degrees, double length, bool thick = false, uint16_t color = BLACK);
 
 public:
     StandardClockFace(Adafruit_GFX &disp) : display(disp)
     {
         w = display.width(), h = display.height();
         mid_x = w / 2, mid_y = h / 2;
+        minorHalfSize = min(w, h) / 2;
+        hour = 0.6 * minorHalfSize, minute = 0.75 * minorHalfSize, second = 0.9 * minorHalfSize;
     }
 
     void initialize();
-    void draw(struct tm const &time, int color);
+    void draw(struct tm const &time, uint16_t color);
 };
 
-void StandardClockFace::DrawHand(double degrees, double length, bool thick, int color)
+void StandardClockFace::DrawHand(double degrees, double length, bool thick, uint16_t color)
 {
     auto x = cos(deg(90 - degrees)) * length;
     auto y = sin(deg(90 - degrees)) * length;
 
-    display.drawLine(mid_x, mid_y, mid_x + x, mid_y + y, BLACK);
+    display.drawLine(mid_x, mid_y, mid_x + x, mid_y + y, color);
     if (thick)
     {
         display.drawLine(mid_x - 1, mid_y, mid_x - 1 + x, mid_y + y, color);
@@ -91,21 +96,16 @@ void StandardClockFace::DrawHand(double degrees, double length, bool thick, int 
 void StandardClockFace::initialize()
 {
     auto i = 5;
-    auto minorHalfSize = min(w, h) / 2;
 
     display.drawRoundRect(i, i, w - 2 * i, h - 2 * i, minorHalfSize / 2, BLACK);
     display.fillCircle(mid_x, mid_y, 5, BLACK);
 }
 
-void StandardClockFace::draw(struct tm const &time, int color)
+void StandardClockFace::draw(struct tm const &time, uint16_t color)
 {
-    auto minorHalfSize = min(w, h) / 2;
-
-    auto hour = 0.6 * minorHalfSize, minute = 0.75 * minorHalfSize, second = 0.9 * minorHalfSize;
-
-    DrawHand(((time.tm_hour % 12) * DegHour) + time.tm_min / 2, hour, true);
-    DrawHand((time.tm_min) * DegMin, minute, true);
-    DrawHand((time.tm_sec) * DegSec, second);
+    DrawHand(((time.tm_hour % 12) * DegHour) + time.tm_min / 2, hour, true, color);
+    DrawHand((time.tm_min) * DegMin + time.tm_sec / 10, minute, true, color);
+    DrawHand((time.tm_sec) * DegSec, second, color);
 }
 
 Adafruit_Iup display(WIDTH, HEIGHT);
